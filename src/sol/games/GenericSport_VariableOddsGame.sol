0707648dda892f8e1e79378669b071a5d09d8390
okajima <okajima@lagarto.co.jp>
2019-02-20 15:43:05 +0900

    game adjustment

--- src/sol/games/GenericSport_VariableOddsGame.sol
+++ src/sol/games/GenericSport_VariableOddsGame.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.19;
 import "../VariableOddsGame_V4_R4.sol";
 
 contract GenericSport_VariableOddsGame is VariableOddsGame_V4_R4 {
-  constructor(string title, address cashier, address voting, address regulation, address owner, uint open_time, uint close_time, bool allow_cancel) public VariableOddsGame_V4_R4(title, cashier, voting, regulation, owner, open_time, close_time, allow_cancel) {
+  constructor(string title, address cashier, address voting, address regulation, address owner, uint open_time, uint close_time, uint bet_total_limit, uint bet_one_time_limit, bool allow_cancel) public VariableOddsGame_V4_R4(title, cashier, voting, regulation, owner, open_time, close_time, bet_total_limit, bet_one_time_limit, allow_cancel) {
     _version = "GenericSport_VariableOddsGame.1";
 
     findOrCreateOdds(0x00010000); //home wins
